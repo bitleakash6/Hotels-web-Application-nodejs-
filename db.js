ca1f4eeca4f88035da44762bc946b8c5bbb0a278
Akash Bitle <bitleakash6@gmail.com>
2024-02-21 16:35:43 +0530

    some changes in db

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 //Define the MangoDb connection url
 
-//const mongoURL = 'mongodb://0.0.0.0:27017/hotels' // Replace 'mydatabase' with your database name
-const mongoURL = process.env.MONGP_DB_URL;
+//const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL,{
